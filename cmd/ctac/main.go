9ee7cac31cfdee3fc1158d8562d2987d9dbe9f46
matilde90 <matilde.aliffi@gmail.com>
2025-09-07 21:08:32 +0100

    Improve logging output

--- cmd/ctac/main.go
+++ cmd/ctac/main.go
@@ -9,14 +9,15 @@ import (
 
 func main (){
 	fmt.Println("Welcome to ctac, critical thinking as code")
+	fmt.Println()
 	file := flag.String("file", "examples/decision.yaml", "path to argument yaml file")
 	flag.Parse()
 
-	data, err := ctac.Loader(*file)
+	argument, err := ctac.Loader(*file)
 	if err != nil {
 		fmt.Printf("cannot unmarshal data: %v", err)
 	}
-	fmt.Println(*data)
-	result:= ctac.RunAllRules(*data)
+	fmt.Println(ctac.SummariseArgument(*argument))
+	result:= ctac.RunAllRules(*argument)
 	fmt.Println(result)
 }
