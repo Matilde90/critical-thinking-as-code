5ec528d1b870b060c51d21b09019ab861beb1213
matilde90 <matilde.aliffi@gmail.com>
2025-11-02 22:09:15 +0000

    Split ctac command in subcommands

--- cmd/ctac/main.go
+++ cmd/ctac/main.go
@@ -9,17 +9,41 @@ import (
 	"os"
 )
 
-func main() {
+func usage() {
+	fmt.Println(`ctac -- Critical Thinking as Code
+	
+	Usage:
+		ctac analyse	[flags]		Analyse an argument file
+		ctac ignore		[subcmd]	Manage ignore file
+		ctac create		[subcmd]	Create argument file
+	
+	Examples:
+		ctac analyse -inputFile file.yaml -outputFile results.md -pretty
+		ctac analyse -inputFile file.yaml parallel -workers 2 -outputFile results.md -pretty
+		ctac ignore print-template
 
-	inputFile := flag.String("inputFile", "", "Path to input argument yaml file")
-	parallel := flag.Bool("parallel", false, "Run rules in parallel - false by default")
-	workers := flag.Int("workers", 3, "Max concurrent workers (only used with parallel flag set as true)")
-	outputFile := flag.String("outputFile", "", "Path to results JSON file")
-	pretty := flag.Bool("pretty", false, "Pretty print JSON")
-	silent := flag.Bool("silent", false, "Quiet mode to silence output written to standard out")
-	ignoreFile := flag.String("ignoreFile", "", "Path to ignore file")
+	Run "ctac <command> -h" for more information about a command.`)
 
-	flag.Parse()
+}
+
+func analyseCmd(args []string) {
+	flagSet := flag.NewFlagSet("analsze", flag.ContinueOnError)
+	flagSet.SetOutput(os.Stderr)
+
+	inputFile := flagSet.String("inputFile", "", "Path to input argument yaml file")
+	parallel := flagSet.Bool("parallel", false, "Run rules in parallel - false by default")
+	workers := flagSet.Int("workers", 3, "Max concurrent workers (only used with parallel flag set as true)")
+	outputFile := flagSet.String("outputFile", "", "Path to results JSON file")
+	pretty := flagSet.Bool("pretty", false, "Pretty print JSON")
+	silent := flagSet.Bool("silent", false, "Quiet mode to silence output written to standard out")
+	ignoreFile := flagSet.String("ignoreFile", "", "Path to ignore file")
+
+	if err := flagSet.Parse(args); err != nil {
+		if err == flag.ErrHelp {
+			return
+		}
+		os.Exit(2)
+	}
 
 	log.SetFlags(0)
 
@@ -82,5 +106,28 @@ func main() {
 		if err := os.WriteFile(*outputFile, b, 0o644); err != nil {
 			log.Fatalf("Write outputfile: %v", err)
 		}
+	}
+}
+
+func main() {
+	log.SetFlags(0)
+	log.SetOutput(os.Stderr)
+	fmt.Println(os.Args)
+	if len(os.Args) < 2 {
+		usage()
+		os.Exit(2)
+	}
+
+	switch os.Args[1] {
+	case "analyze", "analyse":
+		analyseCmd(os.Args[2:])
+	case "ignore":
+		fmt.Print("ignore")
+	case "create":
+		fmt.Print("create")
+	case "help", "-h", "--help":
+		usage()
+	default:
+		fmt.Fprintf(os.Stderr, "unknown command %q\n\n", os.Args[1])
 	}
 }
